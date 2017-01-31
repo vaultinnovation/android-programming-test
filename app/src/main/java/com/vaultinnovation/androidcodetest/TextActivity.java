914e20eec76a734a0a8ab6680d2fca6d27f23529
jj30 <you@example.com>
2017-01-30 20:32:45 -0500

    Finishing test

--- app/src/main/java/com/vaultinnovation/androidcodetest/TextActivity.java
+++ app/src/main/java/com/vaultinnovation/androidcodetest/TextActivity.java
@@ -1,13 +1,21 @@
 package com.vaultinnovation.androidcodetest;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class TextActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text);
+
+        TextView tv = (TextView) findViewById(R.id.message);
+
+        Intent intent = this.getIntent();
+        String strText = intent.getStringExtra("text");
+        tv.setText(strText);
     }
 
 }
